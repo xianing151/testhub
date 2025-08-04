d1774386b10f68771d0832a52fa758b596475583
xianing151 <3918789296@qq.com>
2025-08-04 23:03:31 +0800

    Update SN_hub.lua

--- SN_hub.lua
+++ SN_hub.lua
@@ -1,7 +1,7 @@
 
 local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/FUEx0f3G"))()
 
-local Window = OrionLib:MakeWindow({Name = "SN脚本中心 V1.0.0", HidePremium = false, SaveConfig = true,IntroText = "welcome", ConfigFolder = "Yungengxin制作"})
+local Window = OrionLib:MakeWindow({Name = "SN脚本中心 bata", HidePremium = false, SaveConfig = true,IntroText = "welcome", ConfigFolder = "Yungengxin制作"})
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "HUB"; Text ="HUB"; Duration = 4; })
 
 local about = Window:MakeTab({
@@ -67,8 +67,8 @@ Tab:AddTextbox({
 
 	Callback = function(Value)
 
-          game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-
+         -- game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+            print(Value)
 	end
 
 })
@@ -93,6 +93,66 @@ Tab:AddButton({
 	Name = "退出游戏",
 	Callback = function()
      game.Players.LocalPlayer:Kick("👻👻👻")
+        end
+})
+
+Tab:AddButton({
+	Name = "飞行",
+	Callback = function()
+     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+        end
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "其他脚本",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+Tab:AddButton({
+	Name = "落叶中心",
+	Callback = function()
+     getgenv().LS="落叶中心" loadstring(game:HttpGet("https://raw.githubusercontent.com/krlpl/Deciduous-center-LS/main/%E8%90%BD%E5%8F%B6%E4%B8%AD%E5%BF%83%E6%B7%B7%E6%B7%86.txt"))()
+        end
+})
+
+Tab:AddButton({
+	Name = "霖溺",
+	Callback = function()
+     LnScript = "霖溺-免费版加载器"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShenJiaoBen/ScriptLoader/refs/heads/main/Linni_FreeLoader.lua"))()
+        end
+})
+
+Tab:AddButton({
+	Name = "Rb脚本",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Yungengxin/roblox/refs/heads/main/Rb-Hub"))()
+        end
+})
+
+Tab:AddButton({
+	Name = "皮脚本",
+	Callback = function()
+     getgenv().XiaoPi="皮脚本QQ群1002100032" loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaopi77/xiaopi77/main/QQ1002100032-Roblox-Pi-script.lua"))()
+        end
+})
+
+Tab:AddButton({
+	Name = "Z脚本",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/ZIONPCE/raw/refs/heads/main/ZiO.lua"))()
+        end
+})
+
+Tab:AddButton({
+	Name = "宿傩",
+	Callback = function()
+     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6d74b9cd32ce7fa235684299fd1c79b7.lua"))()
         end
 })
 
@@ -129,5 +189,83 @@ Tab:AddButton({
 SaveNewSettings = true 
 script_key = "DonjoSx_Was_Not_Here" -- Script key, don't remove this
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Umbrella-Scripter/Deadrails-Script/refs/heads/main/average-bond-farm.lua"))()
+        end
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "动感星期五",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "伐木大亨",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+Tab:AddButton({
+	Name = "LuaWare(汉)",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/UWU/main/LuaWare.lua", true))()
+        end
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "在披萨店工作",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+Tab:AddButton({
+	Name = "自动工作脚本",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xianing151/testhub/main/psd.lua"))()
+        end
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "doors",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+Tab:AddButton({
+	Name = "doors最强脚本汉化",
+	Callback = function()
+     loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\54\53\84\119\84\56\106\97"))()
+        end
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "忍者传奇",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+Tab:AddButton({
+	Name = "狐--忍着传奇",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xianing151/testhub/main/狐忍者.lua"))()
         end
 })
