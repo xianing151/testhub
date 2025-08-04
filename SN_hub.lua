fad17bd35b9f5a6178b3d65603228c97d9b6b348
xianing151 <3918789296@qq.com>
2025-08-04 18:45:38 +0800

    Update SN_hub.lua

--- SN_hub.lua
+++ SN_hub.lua
@@ -1,1 +1,59 @@
 
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/FUEx0f3G"))()
+
+local Window = OrionLib:MakeWindow({Name = "SN脚本中心 V1.0.0", HidePremium = false, SaveConfig = true,IntroText = "Yungengxin制作", ConfigFolder = "Yungengxin制作"})
+game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "HUB"; Text ="HUB"; Duration = 4; })
+
+local about = Window:MakeTab({
+    Name = "公告",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+about:AddParagraph("脚本为免费脚本 请勿去圈钱")
+about:AddParagraph("禁止倒卖")
+
+local Tab =Window:MakeTab({
+	Name = "公告",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "复制作者QQ",
+	Callback = function()
+     setclipboard("3918789296")
+  	end
+})
+
+Tab:AddButton({
+	Name = "复制SN脚本交流群QQ",
+	Callback = function()
+     setclipboard("1018099361")
+  	end
+})
+
+OrionLib:MakeNotification({
+	Name = "SN脚本中心",
+	Content = "欢迎使用SN-hub",
+	Image = "rbxassetid://4483345998",
+	Time = 2
+
+})
+
+local Tab = Window:MakeTab({
+
+    Name = "通用",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
+
+local Section = Tab:AddSection({
+
+	Name = "缝合"
+
+})
+
